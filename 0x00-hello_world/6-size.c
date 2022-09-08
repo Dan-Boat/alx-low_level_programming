5a02cd5c9a1f07adc92b8b77cc7393d72940b8b9
Daniel Boateng <dannboateng@gmail.com>
2022-09-08 13:47:48 -0700

    fix bugs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,5 @@ int main(void)
 	printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
 
-	return (0)
+	return (0);
 }
