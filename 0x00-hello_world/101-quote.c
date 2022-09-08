51c76015229439825308113d7a599fe7aa139e1a
Daniel Boateng <dannboateng@gmail.com>
2022-09-08 14:15:12 -0700

    task 8 trial 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints text without using printt or puts 
+ *
+ *  Return: Always 1 (Sucess)
+ *
+ */
+int main(void)
+{
+	// declare and initialize string 
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int s = strlen(text);
+	write(2, text, s);
+	
+	return (1);
+}
