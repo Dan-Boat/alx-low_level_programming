c3f2bfa04beab0a122514c94f044d39a653aae13
Daniel Boateng <dannboateng@gmail.com>
2022-09-08 13:23:55 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: The program just prints text using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
