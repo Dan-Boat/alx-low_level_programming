c80b6c151d78303e01fe1fb6f289a57eda131d7b
Daniel Boateng <dannboateng@gmail.com>
2022-09-08 13:41:41 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry ponit
+ *
+ * Description: Prints the size of the various data types in C
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
+
+	return (0)
+}
