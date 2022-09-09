dc6092f24268c7e2648567b22fe1a25ce0b3a8d1
Daniel Boateng <dannboateng@gmail.com>
2022-09-09 15:55:05 -0700

    task 1,2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *  Description: a program that prints the alphabet in the lowercase
+ *
+ *   Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++
+	}
+	putchar(10);
+
+	return (0);
+}
