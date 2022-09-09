65d856dbf84dba3884eaa73cf1bf9c738b44b527
Daniel Boateng <dannboateng@gmail.com>
2022-09-09 16:17:07 -0700

    fix bugs in task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,27 +2,27 @@
 /**
  * main - Entry points
  *
- * Description: Prints alphabet in lowercase and then uppercase, followed by new line
+ * Description: Prints alphabet in lowercase and 
+ * then uppercase, followed by new line
  *
  * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-	char a='a', b='A';
+	char a = 'a';
+        char b = 'A';
 
 	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
 	}
-	
 	while (b <= 'Z')
 	{
 		putchar(b);
 		b++;
 	}
-	
 	putchar('\n');
 
 	return (0);
