cfda7469756982e98d81e1d6385339640db915a1
Daniel Boateng <dannboateng@gmail.com>
2022-09-09 16:36:33 -0700

    task 4&5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Use putchar funtion to print all letters but the letter 'q' and 'e'.
+ * 
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all numbers of base 10, starting from 0.
+ * 
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
