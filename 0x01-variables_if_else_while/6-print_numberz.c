88f3fe12841d218d91348cd3f7c852bcbe15d373
Daniel Boateng <dannboateng@gmail.com>
2022-09-09 17:06:36 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all single digit 
+ * numbers of base 10 starting from 0
+ *
+ * return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
