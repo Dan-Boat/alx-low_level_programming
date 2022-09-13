7f4f43a9a7dc87f02737adf1c4824b5331785c37
Daniel Boateng <dannboateng@gmail.com>
2022-09-13 14:04:40 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* funtion that prints alphabets in lowercase
+* Retun: 0
+*/
+void print_alphabet(void)
+{
+	char start = 'a';
+
+	while (start <= 'z')
+	{
+		_putchar(start);
+		start++;
+	}
+	_putchar('\n');
+}
