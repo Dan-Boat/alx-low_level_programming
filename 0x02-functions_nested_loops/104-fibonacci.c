d2a87762d83067d67834939e84bfed08856e9e91
Daniel Boateng <dannboateng@gmail.com>
2022-09-13 16:26:57 -0700

    betty fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - entry point 
+* main - entry point
 * Description: finds and prints the first 98 Fibonacci numbers,
 * starting with 1 and 2
 * followed by a new line
