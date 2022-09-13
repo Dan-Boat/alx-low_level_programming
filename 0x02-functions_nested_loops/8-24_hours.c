ba05e303d12a1515960ed7a38ed30b3c9dc18f64
Daniel Boateng <dannboateng@gmail.com>
2022-09-13 15:19:52 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the
+ * day of Jack Bauer, starting from 00:00 to 23:59
+ * Description: prints a list of time
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
