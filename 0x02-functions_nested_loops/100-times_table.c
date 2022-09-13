cd8632c3efeace115d4972f2c4be8e2f0fbd5b3a
Daniel Boateng <dannboateng@gmail.com>
2022-09-13 16:09:07 -0700

    fix betty in 100-

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * If n is greater than 15 or less than 0 the function 
+ * If n is greater than 15 or less than 0 the function
  * should not print anything
  * @n: number of the times table
  */
