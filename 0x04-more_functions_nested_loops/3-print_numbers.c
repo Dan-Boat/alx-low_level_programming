e68a9421cb2eccb415c76fb7855f593f0813f0e7
Daniel Boateng <dannboateng@gmail.com>
2022-09-15 23:57:10 +0200

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - print numbers from 0 to 9
+*/
+void print_numbers(void)
+{
+	int i;
+
+	int 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++
+	}
+	_putchar('\n');
+}
