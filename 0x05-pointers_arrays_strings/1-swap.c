d1a0cb95595f48245d968a3e0139050c471b7831
Daniel Boateng <dannboateng@gmail.com>
2022-09-19 23:21:40 +0200

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+/**
+* swap_int - funtion that swaps the values of two integers
+* @a: parameter int pointer
+* @b: parameter int pointer
+* Return: 0 - Always
+*/
+void swap_int(int *a, int *b)
+{
+		int c;
+
+		c = *a;
+		*a = *b;
+		*b = c;
+}
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,4 +3,6 @@ This exercises  shows how to use pointers and arrays in C.
 
 * 0-reset_to_98.c: function that takes a pointer to an int as parameter and updates the value it points to to 98
 
-*
+* 1-swap.c: function that swaps the values of two integers
+
+* 
