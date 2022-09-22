3ef90920917a84ff0241b61fca9a360209ac154e
Daniel Boateng <dannboateng@gmail.com>
2022-09-22 23:11:31 +0200

    fix cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,10 +26,10 @@ char *cap_string(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] -= 32;
-				trigger = 0;
+				option = 0;
 			}
 			else if (str[i] >= 'A' && str[i] <= 'Z')
-				trigger = 0;
+				option = 0;
 			else if (str[i] >= '0' && str[i] < '9')
 				option = 0;
 		}
