d1cbc10426617b42a8abd93b40ed117ba9c420f9
Daniel Boateng <dannboateng@gmail.com>
2022-09-27 00:41:32 +0200

    fix diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@ void print_diagsums(int *a, int size)
 {
 	int i, num_points, sum1 = 0, sum2 = 0;
 
-	num_points = size * size /* the number of points*/
+	num_points = size * size; /* the number of points*/
 
 	for (i = 0; i < num_points; i++)
 	{
@@ -18,5 +18,5 @@ void print_diagsums(int *a, int size)
 		if (i % (size - 1) == 0 && i != 0 && i < num_points - 1)
 			sum2 += *(a + i);
 	}
-	print("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
