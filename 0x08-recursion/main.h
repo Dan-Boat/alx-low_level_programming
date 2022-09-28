7c3f5d1d4390a8703c0a8f0d0d24aa15d28302e1
Daniel Boateng <dannboateng@gmail.com>
2022-09-29 00:59:48 +0200

    trial advance

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,8 @@ int get_square(int c, int i);
 int is_prime_number(int n);
 int _sqrt(int x, int i);
 int hai(int n, int d);
-
+int is_palindrome(char *s);
+int get_length(char *s);
+int my_pal(char *s, int l);
+int wildcmp(char *s1, char *s2);
 #endif /* MAIN_H */
