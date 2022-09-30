5f046631ebb04108b91e43bc4ac5d262edec4500
Daniel Boateng <dannboateng@gmail.com>
2022-10-01 00:27:12 +0200

    add

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - print the multiplication of two nums
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 -if it works, 1 - fails
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	if (argv[1]  && argv[2])
+	{
+		printf("%d\n", atoi(argv[1] * atoi(argv[2])));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (1);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - print sum of two positive inputs
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 - success, 1 - fails
+*/
+int main(int argc, char *argv[])
+{
+	int sum, i, num;
+	char *c;
+
+	sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &c, 10);
+			if (!*c)
+				sum += num;
+			else
+				printf("Error\n");
+				return (1);
+		}
+	}
+	print("%d\n", sum);
+	return (0);
+}
