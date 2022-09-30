118523e33a9adae75859daf1434bef201396e293
Daniel Boateng <dannboateng@gmail.com>
2022-10-01 01:04:12 +0200

    fix ()

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argv[1]  && argv[2])
 	{
-		printf("%d\n", atoi(argv[1] * atoi(argv[2])));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]);
 		return (0);
 	}
 	else
