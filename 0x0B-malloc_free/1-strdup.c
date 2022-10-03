03558c9c11c62570a2651da2ac083c6ca269d03a
Daniel Boateng <dannboateng@gmail.com>
2022-10-04 00:16:56 +0200

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - function that returns a pointer to a newly
+* allocated space in memory, which contains a copy of the
+* string given as a parameter.
+* @str: string to duplicate
+* Return: Pointer to a the new duped string
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i, c;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
+
+	return (a);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,9 @@
+## Malloc and free for dynamic memory allocation
+
+* 0-create_array.c:  function that creates an array of chars, and initializes it with a specific char
+
+* 1-strdup.c: function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter
+
+* 2-str_concat.c: function that concatenates two strings
+
+* 
