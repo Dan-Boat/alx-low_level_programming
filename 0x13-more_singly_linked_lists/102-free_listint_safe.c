c36dafab5034c0f1e5b04086bd3a6e8bcfdb421a
Daniel Boateng <dannboateng@gmail.com>
2022-10-25 23:42:06 +0200

    fix 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,72 @@
 #include "lists.h"
 
 /**
-* free_listint_safe - free a `listint_t` list and set the head to null
-* @h: double pointer to head of linked list
-* Description: This function should work for circular lists
-* Only loop through the list once
-* Return: size of the list that was free'd
-*/
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
 
-	count = 0;
-	current = *h;
-	while (current != NULL)
+	hptr = NULL;
+	while (*h != NULL)
 	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
+		new = malloc(sizeof(listp_t));
 
-		if (hold < current)
-			break;
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
+		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
 	}
-	*h = NULL;
 
-	return (count);
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
 }
