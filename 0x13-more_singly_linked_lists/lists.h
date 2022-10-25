1548bbd4d2c4f006e2ada70ee104162856482769
Daniel Boateng <dannboateng@gmail.com>
2022-10-26 00:41:35 +0200

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,18 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+/**
+struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
